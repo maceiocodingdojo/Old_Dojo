e871c397062c89f331eb6e0f7d30a6a32aa1ee03
Rachid Calazans <rachidcalazans@gmail.com>
2013-12-07 01:10:56 -0300

    Dojo_ruby_day_1

---
+++ ruby/dojo_day_1/fizz_buzz.rb
@@ -0,0 +1,40 @@
+class FizzBuzz
+
+  def fizzBuzz number
+    mod3 = (number % 3) == 0
+    mod5 = (number % 5) == 0
+    return 'fizzBuzz' if mod3 and mod5
+    return 'fizz' if mod3
+    return 'buzz' if mod5
+    return number
+  end
+  def loop(number)
+    number.times do |i|
+      begin
+        puts fizzBuzz(i+1)
+      rescue
+        return false
+      end
+
+    end
+
+    return true
+  end
+end
+
+require "test/unit"
+class FizzBuzzTest < Test::Unit::TestCase
+  def test_number
+
+    fizzBuzz = FizzBuzz.new
+    assert_equal  1, fizzBuzz.fizzBuzz(1), 'O valor deve ser igual 1'
+    assert_equal 'fizz',fizzBuzz.fizzBuzz(3), 'O retorno deve ser fizz'
+    assert_equal 'buzz', fizzBuzz.fizzBuzz(5), 'O retorno deve ser buzz'
+    assert_equal 'fizzBuzz', fizzBuzz.fizzBuzz(15), 'O retorno deve ser fizzBuzz'
+  end
+
+  def test_execute_loop
+    fizzBuzz = FizzBuzz.new
+    assert_equal true, fizzBuzz.loop(100), "Deve retornar true"
+  end
+end
---
+++ ruby/dojo_day_1/raffle.rb
@@ -0,0 +1,59 @@
+class Raffle
+
+  def self.raffle_name (names)
+    result = String.new
+    puts "size: #{names}"
+    new_names = shuffle_names names
+    new_names.each_with_index do |name, index|
+      result << "#{index + 1}- #{name} "
+    end
+    return result
+
+  end
+
+  def self.shuffle_names(names)
+    new_names = Array.new
+
+    while not names.empty? do
+    #names.size.times do
+      id = Random.rand(names.size)
+      new_names << names[id]
+      names.delete_at(id)
+    end
+
+    return new_names
+
+  end
+
+end
+
+require "test/unit"
+
+class RaffleTest < Test::Unit::TestCase
+
+  def test_verify_type_of_class
+    out = Raffle.raffle_name(get_array)
+    puts "out: #{out}"
+    assert_kind_of String, out, 'Tem que ser do tipo String'
+  end
+
+  def test_shuffle_names
+    input = get_array
+    output = Raffle.shuffle_names input
+
+    assert_kind_of Array, output, 'Tem que ser array'
+
+    input.each do |name|
+      assert_includes output, name, "Tem que ter o nome #{name}"
+    end
+  end
+
+  private
+  def get_array
+    ['bruno', 'rachid', 'irineu', 'danilo']
+  end
+
+
+
+end
+
